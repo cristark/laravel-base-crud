431a1a3463377cec2830bb0b7f654601cbf4886f
Cristiano <cristiano.tarconi@gmail.com>
2021-03-17 22:20:26 +0100

    model, controller, index view

---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,12 @@
+{{-- Estensione layout app principale --}}
+@extends('layouts.app')
+
+{{-- Sezione per aggiornare title --}}
+@section('titolo', 'Lista Auto')
+
+{{-- MAIN --}}
+@section('content')
+    <div class="container">
+        <h1>Lista Auto</h1>
+    </div>
+@endsection
